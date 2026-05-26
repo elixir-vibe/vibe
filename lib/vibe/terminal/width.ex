3ff3e20a54f99881b075a7d0d29a3bee4d4e9d36
Danila Poyarkov <dev@dannote.net>
2026-05-26 23:47:32 +0300

    Use Cringe terminal chunks

--- lib/vibe/terminal/width.ex
+++ lib/vibe/terminal/width.ex
@@ -28,22 +28,6 @@ defmodule Vibe.Terminal.Width do
   def chunks(text, width) do
     text
     |> visible_text()
-    |> String.graphemes()
-    |> Enum.reduce({[], [], 0}, fn grapheme, {chunks, current, used} ->
-      grapheme_width = Cringe.Measure.width(grapheme)
-
-      if used > 0 and used + grapheme_width > width do
-        {[current_to_string(current) | chunks], [grapheme], grapheme_width}
-      else
-        {chunks, [grapheme | current], used + grapheme_width}
-      end
-    end)
-    |> then(fn
-      {chunks, [], _used} -> chunks
-      {chunks, current, _used} -> [current_to_string(current) | chunks]
-    end)
-    |> Enum.reverse()
+    |> Cringe.Measure.chunks(width)
   end
-
-  defp current_to_string(current), do: current |> Enum.reverse() |> Enum.join()
 end
--- mix.exs
+++ mix.exs
@@ -138,7 +138,7 @@ defmodule Vibe.MixProject do
       {:jido, "~> 2.2"},
       {:jido_ai, "~> 2.1"},
       {:ghostty, "~> 0.4.9"},
-      {:cringe, "~> 0.4.0"},
+      {:cringe, "~> 0.5.0"},
       {:ttycast, "~> 0.1.0"}
     ]
   end
